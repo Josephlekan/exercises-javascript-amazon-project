c3d61c7147ae575f46cc90aa9f77f07d34215ca2
josephlekan <josephlekan101@gmail.com>
2026-05-17 00:17:24 +0100

    updated the Delete link

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from "../data/cart.js";
+import {cart, removeCart} from "../data/cart.js";
 import {products} from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -40,7 +40,8 @@ cart.forEach((cartItem) => {
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link js-delete-quantity-link link-primary"
+                    data-product-id = "${matchingProducts.id}">
                     Delete
                   </span>
                 </div>
@@ -97,3 +98,14 @@ cart.forEach((cartItem) => {
 
  document.querySelector('.js-order-summary')
   .innerHTML = checkOutSummary;
+
+  document.querySelectorAll('.js-delete-quantity-link')
+      .forEach((link) => {
+        link.addEventListener('click', () => {
+          const productId = link.dataset.productId;
+          removeCart(productId);
+         
+           });
+      });
+
+ 
